53684b303d6d3c5c518a729701e96435d2f8c0b8
Billy Merchan <billymerchan@gmail.com>
2024-10-21 23:17:11 -0400

    Configure player input using Unity input manager

---
+++ Assets/PlayerInputManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    // Reading input
+    PlayerControls playerControls;
+
+    [SerializeField] Vector2 movementInput;
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+        }
+
+        playerControls.Enable();
+    }
+
+    // Controlling character
+}
