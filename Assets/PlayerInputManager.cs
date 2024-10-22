2bc2ac9a434a85e52deae746a358e77e069c323e
Billy Merchan <billymerchan@gmail.com>
2024-10-21 23:33:55 -0400

    Fix player input not destroying on scene change and disabled during menu

--- Assets/PlayerInputManager.cs
+++ Assets/PlayerInputManager.cs
@@ -1,13 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInputManager : MonoBehaviour
 {
+    public static PlayerInputManager instance;
+
     // Reading input
     PlayerControls playerControls;
 
     [SerializeField] Vector2 movementInput;
+
+    private void Awake(){
+        if (instance == null){
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void Start(){
+
+        DontDestroyOnLoad(gameObject);
+
+        // Runs on scene change
+        SceneManager.activeSceneChanged += OnSceneChange;
+
+        instance.enabled = false;
+        
+    }
+
+    private void OnSceneChange(Scene oldScene, Scene newScene)
+    {
+        // loading into world scene and enabling player inputs/controls
+        // otherwise (menu scene), disable player inputs
+        if (newScene.buildIndex == WorldSaveGameManager.instance.GetWorldSceneIndex())
+        {
+            // If in world scene
+            instance.enabled = true;
+        }
+        else
+        {
+            instance.enabled = false;
+        }
+
+    }
 
     private void OnEnable()
     {
@@ -19,6 +58,12 @@ public class PlayerInputManager : MonoBehaviour
         }
 
         playerControls.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        // if object is destroyed, unsubscribe from this event
+        SceneManager.activeSceneChanged -= OnSceneChange;
     }
 
     // Controlling character
